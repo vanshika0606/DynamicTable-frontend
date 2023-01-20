6751f3f5c3181034aa74aa6067e5b3b8215d5076
Vanshika <vanshika06065@gmail.com>
2023-01-20 15:14:43 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+// import Customer from './component/Customer.js';
+import Order from './component/Order.js';
+import AccountManager from './Pages/accountManager';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <AccountManager/>
+    </div>
+  );
+}
+
+export default App;
